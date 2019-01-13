eef5159966ca02a436b1cea35cf3fdec07e5dd78
Adam Aasma <adam@Adams-iMac.lan>
2019-01-13 19:47:53 +0200

    gitignore

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -22,13 +22,11 @@ export class ProductService {
   }
 
   create(product: any){
-    debugger;
     var fm = new FormData();
     fm.append("Image", product.image);
     fm.append("Name", product.name);
     fm.append("Description", product.description);
     fm.append("Price", product.price);
-    //fm.append('name', product.name);
     
     return this.http.post(`${config.apiUrl}/products/create`, fm);
   }
